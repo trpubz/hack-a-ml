b8424155daf4e928439340dad16bdd3df88d9d39
Taylor "TP" Pubins <tpubz@icloud.com>
2024-02-16 09:51:16 -0700

    Initial Commit

---
+++ hack-a-ml/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hack-a-ml
+//
+//  Created by Taylor Pubins on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hack-a-ml/hack_a_mlApp.swift
@@ -0,0 +1,17 @@
+//
+//  hack_a_mlApp.swift
+//  hack-a-ml
+//
+//  Created by Taylor Pubins on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct hack_a_mlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
